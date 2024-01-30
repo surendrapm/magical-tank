4049259bd1def6c0fa05fe93321a4fad01aeee6e
surendra <suredon456@gmail.com>
2024-01-30 21:23:32 +0530

    added button event functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import Tanks  from './tanks-ui/tanks'
 import './App.css'
 
 function App() {
+
+function calculateTotalWater(){
+  
+}
+
  const TanksContainer = []
  for(let i=0 ; i<4 ; i++){
   TanksContainer.push(<Tanks key={i}/>)
@@ -11,6 +16,7 @@ function App() {
    <>
    <div>
      <div>this is magical tanks</div>
+     <div>Total Water from all tank :- {`${10} liters`}</div>
      <div style={{
        display:'flex',
        marginTop:'5rem'
--- src/tanks-ui/tanks.tsx
+++ src/tanks-ui/tanks.tsx
@@ -1,15 +1,82 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './style.css'
-const tanks = () => {
+
+const Tanks = () => {
+
+const [isclicked,setIsclicked] = useState(false)
+const [currentTankWaterLevel,setcurrentTankWaterLevel] = useState(0)
+const [waterlevel,setWaterlevel] = useState(currentTankWaterLevel)
+const [totalWater , setTotalwater] = useState(0)
+const intervalIdRef  = useRef<NodeJS.Timeout| null>(null)
+
+useEffect(() => {
+   return () => {
+     // Cleanup function to clear interval when component unmounts
+     clearInterval(intervalIdRef.current!);
+   };
+ }, []);
+
+function handleAddWater(){
+    setIsclicked(true)
+    console.log(totalWater)
+   intervalIdRef.current = setInterval(() => {
+      if(totalWater < 1000){
+         setIsclicked(true)
+         setWaterlevel((prevWaterlevel) => prevWaterlevel + 60)
+         setcurrentTankWaterLevel((prevTotalWater) => prevTotalWater + 200)
+      }else {
+         setIsclicked(false);
+         clearInterval(intervalIdRef.current!);
+       }
+         },1000)
+   
+ 
+   }
+     
+
+   const handleMouseUp = () => {
+      clearInterval(intervalIdRef.current!)
+      setIsclicked(false)
+    
+      
+};
+
+
+function handleEmpty(){
+   setWaterlevel(0)
+   setIsclicked(false)
+}
+
   return (<>
               <div>
+               <div>{`${currentTankWaterLevel} liters`}</div>
               <div className='tank-container'>
-                <div className='water'></div>
-              </div>
+                
+                    {isclicked &&
+                          
+                          <div style={{
+                           position: 'relative' ,
+                           top: '0%',
+                           height: `${waterlevel}px`,
+                           width: '250px',
+                           backgroundColor:'aqua',
+                           transition: 'height 0.5s ease-in-out'
+                          }}></div>
+                          
+                    }
+                     
+                      </div>
              <div>
-                <button className='addbtn'>Add water</button>
+                <button 
+                   className='addbtn'
+                    onMouseDown={handleAddWater}
+                    onMouseUp={handleMouseUp}
+                   >Add water</button>
                 <br />
-                <button className='emptybtn'>Empty</button>
+                <button 
+                   className='emptybtn'
+                   onClick={handleEmpty}
+                   >Empty</button>
              </div>
               </div>
              
@@ -18,4 +85,4 @@ const tanks = () => {
   )
 }
 
-export default tanks
+export default Tanks
